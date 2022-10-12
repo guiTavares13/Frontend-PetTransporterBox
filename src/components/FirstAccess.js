f35f12ec34ca53756645b5fe662950c9145cd978
Guilherme Tavares <guiftt035@outlook.com>
2022-10-12 15:29:36 -0300

    Integração backend/frontend Cadastro

--- src/components/FirstAccess.js
+++ src/components/FirstAccess.js
@@ -1,45 +1,129 @@
 import React from "react";
-import { Text, TextInput, StyleSheet, TouchableOpacity, SafeAreaView } from "react-native";
+import { Text, TextInput, StyleSheet, TouchableOpacity, SafeAreaView, View } from "react-native";
 import { useFonts } from 'expo-font';
 import { useState, setState } from "react";
 import global from '../../global';
 
-
+import { server, showError, showSucess } from "../common";
 
 export default props => {
 
+    var [state = {
+        name: '',
+        lastName: '',
+        email: '',
+        documento: '',
+        phone: '',
+        password: '',
+        stageNew: false
+    }, setState] = useState()
 
-    state = {
-        name: ''
-    }
-    
-    
+
     const [fontsLoaded] = useFonts({
         'Jost-BoldItalic': require('../../assets/fonts/Jost-BoldItalic.ttf'),
         'Jost-Regular': require('../../assets/fonts/Jost-Regular.ttf')
     });
+
+    signup = () => {
+        try{
+            fetch(`${server}/user`, {
+            method: 'POST',
+            body: JSON.stringify({
+                nome: state.nome,
+                documento: state.documento,
+                lastName: state.lastName,
+                email: state.email,
+                phone: state.phone,
+                password: state.password
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+            })
+            .then((response) => response.json())
+            .then((json) => console.log(json));
+            showSucess('Deu bom!')
+        } catch(err){
+            showError(err)
+        }
+    }
+    
+   
+
+  /*  signup = async () => {
+        try {
+            await axios.post(`${server}/user`, {
+                name: state.name,
+                lastName: state.lastName,
+                email: state.email,
+                cpf: state.cpf,
+                celular: state.celular,
+                password: state.password
+            });
+
+            showSucess('Usuário cadastrado!')
+        } catch(err){
+            showError(err)
+        }
+        
+    }*/
+
+   /* signinOrSignup = () => {
+        if(this.state.stageNew){
+            Alert.alert('Sucesso!', 'Criar conta')
+        } else {
+            Alert.alert('Sucesso!', 'Logar')
+        }
+    }*/
     
     return(
         <SafeAreaView style={global.container}>
-            <Text style={styles.text}>Como podemos  {'\n'} {' '} chamar você?</Text>
-            <TextInput style={styles.input}  placeholder="Digite um nome" value={this.state.name}
-            onChangeText={name => this.setState({name})}
-            />
-            
-            {state.name.count == 0 ?
-            <TouchableOpacity 
-                title="Button Access" 
-                style={styles.buttonBloqued}><Text>Confirmar
-                </Text>
-           </TouchableOpacity>
-                :
+            <Text style={styles.text}>Caixa Pet</Text>
+            <Text>{state.stageNew ? 'Crie sua conta': 'Informe seus dados'}</Text>
+            <View>
+                {state.stageNew && 
+                    <TextInput style={styles.input}  placeholder="Primeiro nome" value={state.name}
+                    onChangeText={cName => setState(prevState=>({...prevState,nome:cName}))}
+                    />
+                }
+                {state.stageNew && 
+                   <TextInput style={styles.input}  placeholder="Último nome" value={state.lastName}
+                   onChangeText={cLastName => setState(prevState=>({...prevState,lastName:cLastName}))}
+                   />
+                }
+                
+                <TextInput style={styles.input}  placeholder="E-mail" value={state.email}
+                onChangeText={cEmail => setState(prevState=>({...prevState,email:cEmail}))}
+                />
+                {state.stageNew && 
+                    <TextInput style={styles.input}  placeholder="CPF" value={state.documento}
+                    onChangeText={cDocumento => setState(prevState=>({...prevState,documento:cDocumento}))}
+                    />
+                }
+                {state.stageNew &&
+                    <TextInput style={styles.input}  placeholder="Celular" value={state.phone}
+                    onChangeText={cCelular => setState(prevState=>({...prevState,phone:cCelular}))}
+                    />
+                }
+                <TextInput style={styles.input}  placeholder="Senha"  secureTextEntry={true} value={state.password}
+                onChangeText={cPassword => setState(prevState=>({...prevState,password:cPassword}))}
+                />
+                {state.stageNew &&
+                    <TextInput style={styles.input}  placeholder="Confirmar senha" secureTextEntry={true}
+                    />
+                }
+                
+            </View>
+            <TouchableOpacity onPress={() => setState({stageNew: !state.stageNew})} style={{padding: 10}}>
+                <Text>{state.stageNew ? 'Já possui conta?' : 'Ainda não possui conta?'}</Text>
+            </TouchableOpacity>
            <TouchableOpacity 
                     title="Button Access" 
                     style={styles.buttonAccess}
-                    onPress={() => {props.navigation.navigate('FirstAccessConcluded')}}><Text style={styles.textButtonAccess}>Confirmar 1
+                    onPress={signup}><Text style={styles.textButtonAccess}>
+                        {state.stageNew ? 'Registrar' : 'Entrar'}
                     </Text>
             </TouchableOpacity> 
-            }
         </SafeAreaView>
     );
 }
